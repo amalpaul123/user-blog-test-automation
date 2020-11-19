44c63e43de881d15bef636fcd094095f48e77526
Amal Paul <apaul5@sapient.com>
2020-11-19 08:56:28 +0000

    statusCode check at each step

--- src/builders/blog/users/getUser.js
+++ src/builders/blog/users/getUser.js
@@ -1,12 +1,10 @@
 const helperBuilder = require('../../builder-helper');
 const path = require('path');
 
-global.__baseDir = path.resolve(__dirname, '../..');
-global.__templateDir = path.resolve(__dirname, '../templates/requests');
-
 const getUser = async (userId) => {
-    let url = `https://jsonplaceholder.typicode.com/users/${userId}`;
 
+    let url = `https://jsonplaceholder.typicode.com/users/${userId}`;
+ 
     let reqOpts = {
         url: url,
         method: "GET",
--- src/features/step_definitions/steps.js
+++ src/features/step_definitions/steps.js
@@ -10,89 +10,45 @@ var {When} = require('@cucumber/cucumber');
 var {Then} = require('@cucumber/cucumber');
 var apiResponse;
 
-Given(/^the userId is fetched successfully for the username (.*)$/, async  (userName) => {
-  try{
-  // apiResponse = await blogPage.getUsers();
-  let userList = await user.getUsers();
-  let userId = await user.getUserId(JSON.parse(userList.body),userName);
-  helper.setGlobalVariable('userId',userId);
-
-}
-catch(e){
-  console.log("errorr**********",e);
-}
-// expect(apiResponse.statusCode).to.equal(JSON.parse(statusCode), 'API returned invalid response');
-
-});
-
-
-
-Given(/^the posts by the user (.*) are fetched successfuly$/, async  (user) => {
-  try{
-  let postList = await post.getPosts();
-  var postId = await post.getUsersPostIds(JSON.parse(postList.body),helper.getGlobalVariable('userId'));
-
-}
-catch(e){
-  apiResponse = e;
-  console.log("errorr**********",e);
-}
-// expect(apiResponse.statusCode).to.equal(JSON.parse(statusCode), 'API returned invalid response');
-
-});
-
-When(/^Search for the user with username (.*)$/, async  (userName) => {
-    try{
-    apiResponse = await blogPage.getUser();
-  }
-  catch(e){
-    apiResponse = e;
-  }
-});
-Then(/^I should get status code (.*) correctly$/, (statusCode) => {
-    expect(apiResponse.statusCode).to.equal(JSON.parse(statusCode), 'API returned invalid response');
-  });
-
-
-
 //New 
 
-Given(/^the userId is fetched successfully for the user with (.*) as (.*)$/, async  (key,value) => {
+Given(/^the userId is fetched successfully with status code (.*) for the user with (.*) as (.*)$/, async  (statusCode,key,value) => {
   try{
   // apiResponse = await blogPage.getUsers();
   let queryParam ={
     key: key,
     value:value,
   }
-  let apiResponse = await user.getUserByParam(queryParam);
-  helper.setGlobalVariable('userId',JSON.parse(apiResponse.body)[0].id);
-
+  apiResponse = await user.getUserByParam(queryParam);
 }
 catch(e){
-  console.log("errorr**********",e);
+  console.error("error response from api",e);
+  apiResponse = e;
 }
-// expect(apiResponse.statusCode).to.equal(JSON.parse(statusCode), 'API returned invalid response');
+expect(apiResponse.statusCode).to.be.deep.equal(JSON.parse(statusCode), 'API returned invalid response');
+helper.setGlobalVariable('userId',JSON.parse(apiResponse.body)[0].id);
 
 });
 
-Given(/^the posts are fetched successfully for the user using the userId$/, async () => {
+Given(/^the posts are fetched successfully with status code (.*) for the user using the userId$/, async (statusCode) => {
   try{
   let queryParam ={
     key: "userId",
     value: helper.getGlobalVariable('userId'),
   }
-  let apiResponse = await post.getPostsByParam(queryParam);
-  helper.setGlobalVariable('posts',JSON.parse(apiResponse.body));
+   apiResponse = await post.getPostsByParam(queryParam);
 
 }
 catch(e){
-  console.log("errorr**********",e);
+  console.error("error response from api",e);
+  apiResponse = e;
 }
-// expect(apiResponse.statusCode).to.equal(JSON.parse(statusCode), 'API returned invalid response');
+expect(apiResponse.statusCode).to.be.deep.equal(JSON.parse(statusCode), 'API returned invalid response');
+helper.setGlobalVariable('posts',JSON.parse(apiResponse.body));
 
 });
 
-Given(/^validate if the emails in the comment section are in the proper format for each post$/, async () => {
+Given(/^get comments successfully with status code (.*) and validate if the emails in the comment section are in the proper format for each post$/, async (statusCode) => {
   try{
 
   let posts = helper.getGlobalVariable('posts');
@@ -104,7 +60,7 @@ Given(/^validate if the emails in the comment section are in the proper format f
       key: "postId",
       value: post.id,
     }
-    let apiResponse =  await comment.getCommentsByParam(queryParam);
+    apiResponse =  await comment.getCommentsByParam(queryParam);
     let emails = await comment.getCommentsAttributeValue(JSON.parse(apiResponse.body),"emails");
        for(const email of emails){
         expect(email).to.be.a('string');
@@ -113,9 +69,12 @@ Given(/^validate if the emails in the comment section are in the proper format f
 
 }
 catch(e){
-  console.log("errorr**********",e);
+  console.error("error response from api",e);
+  apiResponse = e;
 }
-// expect(apiResponse.statusCode).to.equal(JSON.parse(statusCode), 'API returned invalid response');
-
+expect(apiResponse.statusCode).to.be.deep.equal(JSON.parse(statusCode), 'API returned invalid response');
 });
 
+Then(/^I should get status code (.*) correctly$/, (statusCode) => {
+  expect(apiResponse.statusCode).to.equal(JSON.parse(statusCode), 'API returned invalid response');
+});
