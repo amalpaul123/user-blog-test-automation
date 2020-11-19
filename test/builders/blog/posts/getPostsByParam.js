bcecb63e780af3768d9a9a5c4ff455e7196a51fc
Amal Paul <apaul5@sapient.com>
2020-11-19 23:55:41 +0000

    removed redundant files

--- test/builders/blog/posts/getPostsByParam.js
+++ test/builders/blog/posts/getPostsByParam.js
@@ -7,14 +7,10 @@ global.__templateDir = path.resolve(__dirname, '../templates/requests');
 const getPostsByParam = async (queryParam) => {
     let url = `https://jsonplaceholder.typicode.com/posts?${queryParam.key}=${queryParam.value}`;
 
-    let reqOpts = {
-        url: url,
-        method: "GET",
-        headers: {
-
-        },
-        rejectUnauthorized: false
+    let inpForTemplate = {
+        url: url
     };
+    const reqOpts = JSON.parse(hbr.getRenderedTemplate(__templateDir,"get.hbs", inpForTemplate));
     return await helperBuilder.invoke(reqOpts);
 }
 module.exports = {
--- test/builders/blog/users/getUserByParam.js
+++ test/builders/blog/users/getUserByParam.js
@@ -7,15 +7,10 @@ global.__templateDir = path.resolve(__dirname, '../templates/requests');
 const getUserByParam = async (queryParam) => {
     let url = `https://jsonplaceholder.typicode.com/users?${queryParam.key}=${queryParam.value}`;
 
-    let reqOpts = {
-        url: url,
-        method: "GET",
-        headers: {
-
-        },
-        rejectUnauthorized: false
+    let inpForTemplate = {
+        url: url
     };
-    
+    const reqOpts = JSON.parse(hbr.getRenderedTemplate(__templateDir,"get.hbs", inpForTemplate));
     return await helperBuilder.invoke(reqOpts);
 }
 module.exports = {
